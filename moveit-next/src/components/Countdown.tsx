8b2cdd674927f7e9c571cf5653d11a7370dae310
Adelmo Santos <adelmojsantos1985@gmail.com>
2021-04-07 21:53:16 -0300

    main
    Criação de Context

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext)
+
     const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
@@ -27,11 +30,12 @@ export function Countdown() {
     useEffect(() => {
         if (isActive && time > 0) {
             countdownTimeout = setTimeout(() => {
-                setTime(time - 1)
+                setTime(time - 1);
             }, 1000)
         } else if (isActive && time === 0) {
-            setHasFinished(true)
-            setIsActive(false)
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
